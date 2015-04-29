07193b3e78f583e7ac2427b63c8d132bd6d4e09a
Zak Estrada <zak@linux.com>
2015-04-28 21:54:15 -0500

    More changes to package.use, hibernate on lidclose

--- sync.sh
+++ sync.sh
@@ -3,3 +3,4 @@ cp /etc/portage/package* ./etc/portage/
 cp /etc/portage/make.conf ./etc/portage/
 cp /etc/conf.d/consolefont ./etc/conf.d/
 cp /usr/src/linux/.config ./kernel_config
+cp /etc/acpi/default.sh ./etc/acpi/
